ace19e741cbef62981a37e0b56b47faeada7f04c
ZERO_P <1520059425@qq.com>
2021-06-25 17:40:42 +0800

    :sparkles:Add 367

---
+++ 367_ValidPerfectSquare.c
@@ -0,0 +1,32 @@
+/*
+* @Author: zero
+* @Date:   2021-06-24 12:23:36
+* @Last Modified time: 2021-06-24 12:40:21
+* @Description: 
+* Given a positive integer num, write a function which returns True if num is a perfect square else False.
+*/
+
+#include <stdio.h>
+
+bool isPerfectSquare(int num){
+	if (num == 0||num == 1)
+		return true;
+	int left = 0, right = num, mid;
+	while (left <= right){
+		mid = left + (right - left) / 2;
+		if (num/mid == mid&&num%mid == 0)
+			return true;
+		else if (num/mid > mid)
+			left = mid + 1;
+		else if (num/mid < mid)
+			right = mid - 1;
+		else
+			return false;
+	}
+	return false;
+}
+
+int main(int argc, char const *argv[]){
+	isPerfectSquare(2) ? printf("T") : printf("F");
+	return 0;
+}
