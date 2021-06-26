62d913eaaa3b2799ba4e886f33c441be08a806ed
ZERO_P <1520059425@qq.com>
2021-06-26 11:33:36 +0800

    :sparkles:Add 26

---
+++ 26_RemoveDuplicatesFromSortedArray.c
@@ -0,0 +1,28 @@
+/*
+* @Author: zero
+* @Date:   2021-06-26 11:14:49
+* @Last Modified time: 2021-06-26 11:31:27
+* @Description: 
+* Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place
+* such that each unique element appears only once. The relative order of the elements should be kept the same.
+* Return k after placing the final result in the first k slots of nums.
+*/
+
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize){
+	if (numsSize == 0)
+		return 0;
+	int slow = 0;
+	for (int fast = 0;fast < numsSize;++ fast){
+		if (nums[slow] != nums[fast])
+			nums[++slow] = nums[fast];
+	}
+	return slow + 1;
+}
+
+int main(){
+	int nums[] = {};
+	printf("%d\n", removeDuplicates(nums, 0));
+	return 0;
+}
