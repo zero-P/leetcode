b345dbfbc76546fb7d41c33763544cbcc9cc3664
zero-P <1520059425@qq.com>
2021-06-30 14:27:30 +0800

    :adhesive_bandage:Release the memory space of the deleted node

--- 203_RemoveLinkedListElements.c
+++ 203_RemoveLinkedListElements.c
@@ -1,7 +1,7 @@
 /*
 * @Author: zero
 * @Date:   2021-06-30 12:23:02
-* @Last Modified time: 2021-06-30 12:42:26
+* @Last Modified time: 2021-06-30 14:22:06
 * @Description: Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
 */
 
@@ -15,10 +15,13 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
     p->next = head;
     struct ListNode *h = p;
     while (p->next) {
-    	if (p->next->val == val)
-   			p->next = p->next->next;
-   		else
-   			p = p->next;
+        if (p->next->val == val) {
+            struct ListNode *q = p->next;
+            p->next = p->next->next;
+            free(q);
+        }
+        else
+            p = p->next;
     }
     return h->next;
 }
