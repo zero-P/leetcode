78f855688e842a3f16fed4e267c3a33469727b48
zero-P <1520059425@qq.com>
2021-07-02 20:47:14 +0800

    :sparkles:Add 217

---
+++ 217_ContainsDuplicate.c
@@ -0,0 +1,40 @@
+/*
+* @Author: zero
+* @Date:   2021-07-02 17:06:11
+* @Last Modified time: 2021-07-02 17:15:38
+* @Description: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+
+#include "uthash.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct hashtable {
+	int key;
+	UT_hash_handle hh;
+};
+
+bool containsDuplicate(int* nums, int numsSize){
+	struct hashtable *set = NULL;
+	for (int i = 0; i < numsSize; i++) {
+		struct hashtable * tmp;
+		HASH_FIND_INT(set, nums + i, tmp);
+		if (!tmp) {
+			tmp = (struct hashtable*)malloc(sizeof(struct hashtable));
+			tmp->key = nums[i];
+			HASH_ADD_INT(set, key, tmp);
+		} else
+			return true;
+	}
+	return false;
+}
+
+int main() {
+	int nums[] = {1, 1, 2, 3};
+	int size = 4;
+	if (containsDuplicate(nums, size))
+		printf("y");
+	else
+		printf("n");
+	return 0;
+}
