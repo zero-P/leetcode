7637e05b27f1ca67183c88fc8c89dce019a3ee63
zero-P <1520059425@qq.com>
2021-07-04 10:02:12 +0800

    :sparkles:Add 142

---
+++ 142_LinkedListCycleII.c
@@ -0,0 +1,27 @@
+/*
+* @Author: zero
+* @Date:   2021-07-04 09:27:01
+* @Last Modified time: 2021-07-04 09:57:40
+* @Description: Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+*/
+
+struct ListNode *detectCycle(struct ListNode *head) {
+	if (!head || !head->next)
+		return NULL;
+	struct ListNode *fast, *slow;
+    fast = slow = head;
+	while (fast && fast->next) {
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			break;
+	}
+	if (slow != fast)
+		return NULL;
+	fast = head;
+	while (fast != slow) {
+		fast = fast->next;
+		slow = slow->next;
+	}
+	return fast;
+}
